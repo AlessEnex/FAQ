00638a7602bb7d6725e24ec8285c1e221d9b7d37
AlessEnex <154332929+AlessEnex@users.noreply.github.com>
2024-03-19 09:13:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const questions = document.querySelectorAll('.faq-list h2');
-  
-  questions.forEach(question => {
-    question.addEventListener('click', function() {
-      const answer = this.nextElementSibling;
-      answer.classList.toggle('show');
+  const toggleButtons = document.querySelectorAll('.toggle-answer');
+
+  toggleButtons.forEach(button => {
+    button.addEventListener('click', function() {
+      console.log("Pulsante cliccato"); // Verifica se il pulsante viene cliccato
+
+      const answer = this.parentElement.nextElementSibling;
+      console.log(answer); // Verifica se l'elemento della risposta è selezionato correttamente
+      
+      if (answer) {
+        answer.classList.toggle('show');
+        console.log(answer.classList); // Verifica se le classi vengono aggiunte/rimosse correttamente
+        this.querySelector('.plus-symbol').textContent = answer.classList.contains('show') ? '-' : '+';
+        console.log("Testo del pulsante cambiato"); // Verifica se il testo del pulsante viene cambiato correttamente
+      } else {
+        console.log("Elemento della risposta non trovato");
+      }
     });
   });
 });
